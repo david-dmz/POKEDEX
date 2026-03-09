92a6a0091bf881882b6c0baed2b728b50f8afe1f
Phil Higgs <phil@debian.debby.com>
2026-03-09 17:14:20 -0400

    ajout correction intra pratique

---
+++ intra_formatif/corrige/src/components/ComponentB/ComponentB.jsx
@@ -0,0 +1,5 @@
+export default function ComponentB() {
+    return (
+        <div className="comp-b" style={{ margin: '5px' }}><span>Option Beta - Actif</span></div>
+    );
+};
---
+++ intra_formatif/corrige/src/components/ComponentC/ComponentC.jsx
@@ -0,0 +1,5 @@
+export default function ComponentC() {
+    return (
+        <div className="comp-c" style={{ margin: '5px' }}><i>Détail Gamma - Vérifié</i></div>
+    )
+}
